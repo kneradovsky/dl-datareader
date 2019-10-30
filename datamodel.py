datamodel custom softmax, predict fixed

datamodel.py
@@ -24,11 +24,17 @@ def softmax(x, axis=1):
     else:
         raise ValueError('Cannot apply softmax to a tensor that is 1D')
 
+
+
+
 def string2int(s, length,vocab):
     s = s.lower()
-    if(len(s)>length) :
+    s=s.replace(',','')
+    if len(s)>length:
         s=s[:length]
-    rep = list(map(lambda x: vocab.get(x, '<unk>'), s))
+    
+    rep = list(map(lambda x: vocab.get(x, vocab.get('<unk>')), s))
+
     if len(s)<length:
         rep += [vocab['<pad>']] * (length - len(s))
     return rep
@@ -72,7 +78,7 @@ class ModelWrapper:
     post_activation_LSTM = None
     output_layer = None
 
-    def one_step_attention(self,a,s_prev,Tx):
+    def one_step_attention(self,a,s_prev):
         s_prev = self.repeator(s_prev)
         concat = self.concatenator([a,s_prev])
         e = self.densor1(concat)
@@ -85,7 +91,7 @@ class ModelWrapper:
         #create repeator layer
         self.repeator = layers.RepeatVector(Tx)
         self.post_activation_LSTM=layers.LSTM(n_s,return_state=True)
-        self.output_layer = layers.Dense(machine_vocab_size,activation='softmax')
+        self.output_layer = layers.Dense(machine_vocab_size,activation=softmax)
 
         X = layers.Input(shape=(Tx,human_vocab_size))
         s0 = layers.Input(shape=(n_s,),name='s0')
@@ -98,11 +104,11 @@ class ModelWrapper:
         a = layers.Bidirectional(layers.LSTM(n_a,return_sequences=True))(X)
 
         for i in range(Ty):
-            context = self.one_step_attention(a,s,Tx)
+            context = self.one_step_attention(a,s)
             s, ss, c = self.post_activation_LSTM(context,initial_state=[s,c])
             output = self.output_layer(s)
             outputs.append(output)
         model = tf.keras.Model(inputs=[X,s0,c0],outputs=outputs)
-        return model;
+        return model
 
 

datatrain.py
@@ -19,12 +19,13 @@ Ty = data['config']['Ty']
 human_vocab = data['human']
 machine_vocab = data['machine']
 inv_machine = data['inv_machine']
+print(machine_vocab)
 
 X=[]
 Y=[]
 for d in tqdm(data['data']):
-    X.append(d[0])
-    Y.append(d[3])
+    X.append(d[0].lower())
+    Y.append(d[3].lower())
 
 m_train = int(0.9*len(X))
 m_test = len(X) - m_train
@@ -39,6 +40,8 @@ X,Y,Xoh,Yoh = preprocess_data(X,Y,human_vocab,machine_vocab,Tx,Ty)
 n_a = 32
 n_s = 64
 
+X_test,Y_test,Xoh_test,Yoh_test = preprocess_data(X_test,Y_test,human_vocab,machine_vocab,Tx,Ty)
+Yoh_test = list(Yoh_test.swapaxes(0,1))
 
 mw = ModelWrapper()
 
@@ -48,28 +51,28 @@ if weigthsfile != None :
     model.load_weights(weigthsfile)
 
 opt = tf.keras.optimizers.Adam(lr=0.005,beta_1=0.9,beta_2=0.999,decay=0.01)
-model.compile(opt,loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(opt,loss='categorical_crossentropy',metrics=['accuracy','recall','precision'])
 
 
 #initialize parameters
 s0 = np.zeros((m,n_s))
 c0 = np.zeros((m,n_s))
+s0test = np.zeros((m_test,n_s))
+c0test = np.zeros((m_test,n_s))
 
 outputs = list(Yoh.swapaxes(0,1))
 
-model.fit([Xoh,s0,c0],outputs,epochs=1,batch_size=100)
+model.fit([Xoh,s0,c0],outputs,epochs=10,batch_size=100,verbose=0,validation_data=[[Xoh_test,s0test,c0test],Yoh_test])
 
 if weigthsfile == None : weigthsfile="dataweights.h5"
 
 model.save_weights(weigthsfile)
 
-X_test,Y_test,Xoh_test,Yoh_test = preprocess_data(X_test,Y_test,human_vocab,machine_vocab,Tx,Ty)
-Yoh_test = list(Yoh_test.swapaxes(0,1))
 
-s0 = np.zeros((m_test,n_s))
-c0 = np.zeros((m_test,n_s))
 
-model.evaluate([Xoh_test,s0,c0],Yoh_test)
+
+
+#model.evaluate([Xoh_test,s0,c0],Yoh_test)
 
 
 model.save('datareadmodel.h5')